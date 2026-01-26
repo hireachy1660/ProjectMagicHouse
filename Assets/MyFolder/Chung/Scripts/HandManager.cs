f19657f315e16905964bba2b04967a87bf4e517a
hireachy1660 <klico1911@gmail.com>
2026-01-26 20:26:58 +0900

    헨드 메니저 디버그 추가

--- Assets/MyFolder/Chung/Scripts/HandManager.cs
+++ Assets/MyFolder/Chung/Scripts/HandManager.cs
@@ -1,16 +1,18 @@
 using Oculus.Interaction;
+using Oculus.Interaction.HandGrab;
 using UnityEngine;
 
 public class HandManager : MonoBehaviour
 {
     // [설정] 인스펙터에서 인터렉터를 미리 넣어둡니다.
     [SerializeField] private RayInteractor _rayInteractor;
-    [SerializeField] private GrabInteractor _handInteractor;
+    [SerializeField] private HandGrabInteractor _handInteractor;
 
 
     // 이 함수를 인터렉터의 [Select Entered] 또는 [Activate] 이벤트에 연결합니다.
     public void OnInteractAction()
     {
+        Debug.Log("[HandManager] On Interact Action is Start");
         // 1. [Select 될 때만 가져옴] 손에 잡고 있는 아이템 확인
         IItem currentItem = null;
         if (_handInteractor.HasInteractable) // Meta SDK 기능 활용
@@ -18,6 +20,7 @@ public class HandManager : MonoBehaviour
             // 잡은 물체의 인터페이스 추출
             var grabbedObj = _handInteractor.Interactable.transform.gameObject;
             currentItem = grabbedObj.GetComponent<IItem>();
+            Debug.Log("Current Grab Item Is :" + currentItem);
         }
 
         // 2. [Select 될 때만 가져옴] 레이가 보고 있는 리시버 확인
@@ -41,5 +44,10 @@ public class HandManager : MonoBehaviour
                 targetReceiver.OnActivate();
             }
         }
+    }
+
+    public void DebugHover()
+    {
+        Debug.Log("[HandManager] OnHover");
     }
 }
