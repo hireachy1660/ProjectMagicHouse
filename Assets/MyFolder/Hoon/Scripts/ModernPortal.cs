eaed67df7ad6449c8b0a8b9e342a8af9aa06ec06
diiabilor123-ops <diiabilor123@gmail.com>
2026-01-29 15:30:51 +0900

    Update ModernPortal.cs

--- Assets/MyFolder/Hoon/Scripts/ModernPortal.cs
+++ Assets/MyFolder/Hoon/Scripts/ModernPortal.cs
@@ -45,6 +45,7 @@ public class ModernPortal : MonoBehaviour
 
 
 
+
         Vector3 eyeWorldPos = Camera.main.GetStereoViewMatrix(eye).inverse.GetColumn(3);
         Quaternion eyeWorldRot = mainCamTransform.rotation;
 
