c61913e187fa63436f3778e1968984a4674061dd
hireachy1660 <klico1911@gmail.com>
2026-02-06 18:16:57 +0900

    머지전 커밋 인벤토리 작업 중

--- Assets/MyFolder/Chung/Scripts/InventoryButton.cs
+++ Assets/MyFolder/Chung/Scripts/InventoryButton.cs
@@ -2,42 +2,65 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class InventoryButton : MonoBehaviour
+public class InventoryButton : MonoBehaviour,IReceiver
 {
-    private IItem myIItem;
     private EvidenceData myData;
+    [SerializeField]
+    private GameObject itemInfoPanel;
     [SerializeField]
     private Image itemImage;
     [SerializeField]
     private TextMeshProUGUI titleTextTMP = null;
 
 
-    public void SetButton(IItem _item, EvidenceData _data)
+    private IItem myIItem;
+
+    public delegate void InvenButtonvoidIItemdelegate(IItem _item, InventoryButton _btnScript = null);
+    private InvenButtonvoidIItemdelegate UseCallback;
+    private InvenButtonvoidIItemdelegate AddCallback;
+    public InvenButtonvoidIItemdelegate OnInvenButtonUseCallback
+    { set { UseCallback = value; } } 
+    public InvenButtonvoidIItemdelegate OnInvenButtonAddCallback
+    { set { AddCallback = value; } }
+
+
+
+
+    public void SetButton(EvidenceData _data)
     {
-        myIItem = _item;
         myData = _data;
 
         itemImage.sprite = myData.icon;
         titleTextTMP.text = myData.title;
 
-        myIItem.Transform.position = new Vector3(0, 100f, 0);
     }
 
-    public void OnClickInvenButton()
+    public void OnReceiveItem(IItem _item)
     {
-        myIItem.Transform.position = transform.position;
-        this.gameObject.SetActive(false);
-
+            myIItem = _item;
+            AddCallback?.Invoke(myIItem,this);
+            SetPanelActive(true);
     }
 
-    public void UseItem()
+    public void OnActivate()
     {
+        if (myIItem == null || itemInfoPanel.activeSelf) return;
 
+        UseCallback?.Invoke(myIItem, this);
+        SetPanelActive(false);
     }
 
-    private void AddItem()
+    public void SetPanelActive(bool _panelActive)
     {
+        itemInfoPanel.SetActive(_panelActive);
+    }
 
+    public void ClearMyInfo()
+    {
+        myIItem = null;
+        myData = null;
+        itemImage.sprite = null;
+        titleTextTMP.text = string.Empty;
     }
 
 }
